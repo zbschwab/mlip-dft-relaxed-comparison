af992208506925300ce96769b99977d7651630ec
Z Schwab <zbajraktarischwab@gmail.com>
2026-06-08 18:28:00 -0500

    tested all models and edited filepath template for CH3CHO

--- model-scripts/run_mace.py
+++ model-scripts/run_mace.py
@@ -15,7 +15,12 @@ from mace.calculators import mace_mp
 
 ads_slab = read(sys.argv[1], format="vasp")
 
+# set MACE to run on mps instead of cpu 
+# (change depending on your particular gpu predicament)
 calc = mace_mp(model="medium", dispersion=False, default_dtype="float32")
+calc.models = [m.float().to("mps") for m in calc.models]
+calc.device = "mps"
+
 ads_slab.calc = calc
 
 opt = BFGS(ads_slab, trajectory=sys.argv[2])
