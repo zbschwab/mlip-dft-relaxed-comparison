5d4b950b3c130d037f9caa82dd4ced9f99b205e4
Z Schwab <zbajraktarischwab@gmail.com>
2026-06-11 15:03:50 -0500

    refactored to take device type as param

---
+++ YPdCu_slab/model-scripts/run_mace.py
@@ -0,0 +1,31 @@
+"""
+run_mace.py — run in mlip-mace environment
+conda activate mlip-mace
+"""
+
+import sys
+from ase.io import read
+from ase.optimize import BFGS
+from mace.calculators import mace_mp
+
+# print("hello from mace!")  # check connection
+
+# argv[1]: path to dft run POSCAR
+# argv[2]: path to write mlip run to
+# argv[3]: device type to run model with. (inherited from PyTorch)
+#   params:
+#       cpu  : default, runs on most cpus
+#       cuda : use with nvidia or AMD gpus
+#       mps  : use with an apple M-series chip
+
+ads_slab = read(sys.argv[1], format="vasp")
+
+# set MACE to run on mps instead of cpu
+# (change depending on your particular gpu predicament)
+calc = mace_mp(model="medium", dispersion=False, default_dtype="float32")
+calc.models = [m.float().to("mps") for m in calc.models]
+calc.device = sys.argv[3]
+ads_slab.calc = calc
+
+opt = BFGS(ads_slab, trajectory=sys.argv[2])
+opt.run(fmax=0.05)  # converge forces to 0.05 eV/Å
---
+++ YPdCu_slab/model-scripts/run_mattersim.py
@@ -0,0 +1,26 @@
+"""
+run_mattersim.py — run in mlip-mattersim environment
+conda activate mlip-mattersim
+"""
+
+import sys
+from ase.io import read
+from ase.optimize import BFGS
+from mattersim.forcefield import MatterSimCalculator
+
+# print("hello from mattersim!")  # check connection
+
+# argv[1]: path to dft run POSCAR
+# argv[2]: path to write mlip run to
+# argv[3]: device type to run model with. params:
+#       cpu  : default, runs on most cpus
+#       cuda : use with a nvidia gpu
+
+ads_slab = read(sys.argv[1], format="vasp")
+
+calc = MatterSimCalculator(device=sys.arg[3])
+
+ads_slab.calc = calc  # no mps option, runs on cpu
+
+opt = BFGS(ads_slab, trajectory=sys.argv[2])
+opt.run(fmax=0.05)  # converge forces to 0.05 eV/Å
